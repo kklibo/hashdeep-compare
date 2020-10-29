d84974722a81ad293c9b61fe5e9f93162e76ba6a
kklibo <kklibo4@gmail.com>
2020-10-28 17:29:19 -0700

    removed obsolete command::get_hashdeep_command

--- src/command.rs
+++ src/command.rs
@@ -1,10 +1,5 @@
 use std::process::{Command,Stdio,ExitStatus};
 
-pub fn get_hashdeep_command(target_directory: &str, output_path_base: &str) -> String {
-
-    format!( "hashdeep -lro f \"{0}\" > \"{1}\" 2> \"{1}.errors\"", target_directory, output_path_base)
-}
-
 pub fn run_hashdeep_command(target_directory: &str, output_path_base: &str) -> std::io::Result<ExitStatus> {
 
     use std::fs::File;
@@ -21,24 +16,4 @@ pub fn run_hashdeep_command(target_directory: &str, output_path_base: &str) -> s
     .stderr(File::create(format!("{}.errors", output_path_base))?)
 
     .status()
-}
-
-#[cfg(test)]
-mod test
-{
-    use super::*;
-
-    #[test]
-    fn get_hashdeep_command_test() {
-
-        assert_eq!(r#"hashdeep -lro f "." > "~/filename with spaces" 2> "~/filename with spaces.errors""#,
-            get_hashdeep_command(".", "~/filename with spaces")
-        );
-
-        assert_eq!(r#"hashdeep -lro f "/mnt/usb1/" > "/mnt/usb2/outfile" 2> "/mnt/usb2/outfile.errors""#,
-            get_hashdeep_command("/mnt/usb1/", "/mnt/usb2/outfile")
-        );
-
-    }
-
 }
