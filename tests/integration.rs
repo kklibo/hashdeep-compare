b0bce1ae90d380e3f382a8e473ce0feb5dac7091
kklibo <kklibo4@gmail.com>
2020-12-17 15:39:06 -0800

    added test: 1_full_match_group_in_both_files

--- tests/integration.rs
+++ tests/integration.rs
@@ -146,6 +146,7 @@ fn structured_integration_tests() -> Result<(), Box<dyn std::error::Error>> {
     part_test("1_full_match_pair")?;
     part_test("1_full_match_group_in_file1_only")?;
     part_test("1_full_match_group_in_file2_only")?;
+    part_test("1_full_match_group_in_both_files")?;
 
 
 
