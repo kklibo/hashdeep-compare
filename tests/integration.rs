3e296ade0477a253bdf205259e75425ba5b2c9bc
kklibo <kklibo4@gmail.com>
2020-12-17 21:32:30 -0800

    added test: no_entries

--- tests/integration.rs
+++ tests/integration.rs
@@ -170,6 +170,7 @@ fn structured_integration_tests() -> Result<(), Box<dyn std::error::Error>> {
     part_test("1_entry_in_file1_with_no_match")?;
     part_test("1_entry_in_file2_with_no_match")?;
 
+    part_test("no_entries")?;
 
 
     fn run_test (subdir: &str, args: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
