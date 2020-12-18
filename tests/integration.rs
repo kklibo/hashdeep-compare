14d70c529faf002ac17e0fdd34e3ed0dd7cc0dc3
kklibo <kklibo4@gmail.com>
2020-12-17 16:42:36 -0800

    added test: 1_hashes_match_group_in_file1_only

--- tests/integration.rs
+++ tests/integration.rs
@@ -154,6 +154,7 @@ fn structured_integration_tests() -> Result<(), Box<dyn std::error::Error>> {
     part_test("1_name_match_group_in_both_files")?;
 
     part_test("1_hashes_match_pair")?;
+    part_test("1_hashes_match_group_in_file1_only")?;
 
 
     fn run_test (subdir: &str, args: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
