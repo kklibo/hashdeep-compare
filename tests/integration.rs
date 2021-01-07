5d9060a561edaf3b0dda7eab9b7be3552d7df262
kklibo <kklibo4@gmail.com>
2021-01-07 11:01:59 -0800

    fixed test + removed redundant integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -16,58 +16,6 @@ use pathdiff::diff_paths;
 
 const BIN_NAME: &str = env!("CARGO_PKG_NAME");
 
-fn prints_help_message_fn(args: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
-
-    Command::cargo_bin(BIN_NAME)?
-        .args(args)
-        .assert().stdout(predicates::str::contains("hashdeep-compare version"));
-
-    Ok(())
-}
-
-#[test]
-fn no_parameters() -> Result<(), Box<dyn std::error::Error>> {
-
-    prints_help_message_fn(&[])
-
-}
-
-#[test]
-fn invalid_parameters() -> Result<(), Box<dyn std::error::Error>> {
-
-    Command::cargo_bin(BIN_NAME)?
-        .arg("nonexistent_command")
-        .assert().stderr(predicates::str::contains("invalid command"));
-
-    Ok(())
-}
-
-
-fn sort_success_fn(in_file: &str, expected_file: &str) -> Result<(), Box<dyn std::error::Error>> {
-
-    let temp_file = NamedTempFile::new()?;
-
-    Command::cargo_bin(BIN_NAME)?
-        .arg("sort")
-        .arg(in_file)
-        .arg(temp_file.path())
-        .assert().success();
-
-    let p = predicates::path::eq_file(Path::new(expected_file));
-    assert!(p.eval(temp_file.path()));
-
-    Ok(())
-}
-
-
-#[test]
-fn sort_success() -> Result<(), Box<dyn std::error::Error>> {
-
-    sort_success_fn("tests/test1.txt", "tests/test1 sorted.txt")?;
-
-    Ok(())
-}
-
 
 #[test]
 fn hash_success() -> Result<(), Box<dyn std::error::Error>> {
@@ -127,7 +75,8 @@ fn structured_integration_tests() -> Result<(), Box<dyn std::error::Error>> {
 
 
     //invalid subcommand tests
-    run_test("invalid/0_arguments",             &[""])?;
+    run_test("invalid/0_arguments",             &[])?;
+    run_test("invalid/empty_argument",          &[""])?;
     run_test("invalid/nonexistent_subcommand",  &["nonexistent_subcommand"])?;
 
 
