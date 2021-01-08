f728ae81f6e2f04637dbec70b5553e65ccd78186
kklibo <kklibo4@gmail.com>
2021-01-08 12:58:06 -0800

    existing "sort" output file now handled as error + updated tests

--- src/sort.rs
+++ src/sort.rs
@@ -1,7 +1,11 @@
 use common;
 use log_entry::LogEntry;
 
-pub fn sort_log(filename: &str, out_filename: &str) -> ::std::io::Result<()>{
+pub fn sort_log(filename: &str, out_filename: &str) -> Result<(), Box<dyn std::error::Error>>{
+
+    if std::path::Path::exists(out_filename.as_ref()) {
+        return Err(format!("{} exists (will not overwrite existing files)", out_filename).into());
+    }
 
     let mut log_file = common::read_log_entries_from_file::<Vec<LogEntry>>(filename)?;
     assert_eq!(0, log_file.invalid_lines.len());//todo: remove this
@@ -11,7 +15,8 @@ pub fn sort_log(filename: &str, out_filename: &str) -> ::std::io::Result<()>{
         v1.filename.cmp(&v2.filename)
     });
 
-    common::write_log_entries_to_file(log_file.entries, out_filename)
+    common::write_log_entries_to_file(log_file.entries, out_filename)?;
+    Ok(())
 }
 
 #[cfg(test)]
@@ -27,13 +32,14 @@ mod test {
     #[test]
     fn sort_log_test() {
         {
-            let temp_file = NamedTempFile::new().unwrap();
-            let temp_file_path_str = temp_file.path().to_str().unwrap();
+            let temp_dir = tempfile::tempdir().unwrap();
+            let temp_file = temp_dir.path().join("test1 sorted.txt");
+            let temp_file_path_str = temp_file.to_str().unwrap();
 
             sort_log("tests/test1.txt", temp_file_path_str).unwrap();
 
             let p = predicates::path::eq_file("tests/test1 sorted.txt");
-            assert!(p.eval(temp_file.path()));
+            assert!(p.eval(temp_file.as_path()));
         }
     }
 }
