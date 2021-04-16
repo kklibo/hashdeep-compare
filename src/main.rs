7fe69f224366edf1332b67a9dd21d8912759d8cc
kklibo <kklibo4@gmail.com>
2021-04-16 15:57:25 -0700

    writeln used for stderr output

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,38 @@ use std::error::Error;
 use std::io::{stdout,stderr,Write};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    main_impl(Box::new(stdout()))
+
+    let return_code =
+    match main_impl(Box::new(stdout()))
+    {
+        Ok(()) => 0,
+        Err(err) => {
+            writeln! (stderr(), "Error: {:?}", err)?;
+            1
+        }
+    };
+
+    std::process::exit(return_code);
 }
 
 fn main_impl(mut stdout: Box<dyn Write>) -> Result<(), Box<dyn Error>> {
 
     const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-    let mut show_help = ||{
-        writeln!(stdout, "hashdeep-compare version {}", VERSION);
-        writeln!(stdout, " arguments");
-        writeln!(stdout, "  version");
-        writeln!(stdout, "  hash target_directory output_path_base");
-        writeln!(stdout, "  sort input_file output_file");
-        writeln!(stdout, "  part input_file1 input_file2 output_file_base");
+    let mut show_help = || -> Result<(), Box<dyn Error>> {
+        writeln!(stdout, "hashdeep-compare version {}", VERSION)?;
+        writeln!(stdout, " arguments")?;
+        writeln!(stdout, "  version")?;
+        writeln!(stdout, "  hash target_directory output_path_base")?;
+        writeln!(stdout, "  sort input_file output_file")?;
+        writeln!(stdout, "  part input_file1 input_file2 output_file_base")?;
+        Ok(())
     };
 
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
-        show_help();
+        show_help()?;
         return Ok(());
     }
 
@@ -48,10 +60,10 @@ fn main_impl(mut stdout: Box<dyn Write>) -> Result<(), Box<dyn Error>> {
             let partition_stats =
             partition::partition_log(args[2].as_str(), args[3].as_str(), args[4].as_str())?;
 
-            writeln!(stdout, "{}", partition_stats);
+            writeln!(stdout, "{}", partition_stats)?;
         },
         "version" => {
-            writeln!(stdout, "hashdeep-compare version {}", VERSION);
+            writeln!(stdout, "hashdeep-compare version {}", VERSION)?;
         },
 
         x => return Err(format!("invalid command: {}", x).into())
