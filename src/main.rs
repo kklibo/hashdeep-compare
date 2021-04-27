c933cb67db762bb055de091a7a74191d66e98eca
kklibo <kklibo4@gmail.com>
2021-04-27 14:24:57 -0700

    converted block comments to line comments

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ use std::error::Error;
 use std::io::{stdout,stderr};
 use hashdeep_compare::main_impl::main_io_wrapper;
 
-/**
-Runs the program: directs program arguments, stdout, and stderr to main_impl::main_io_wrapper,
-and exits with the resulting exit code or error
-
-See main_impl.rs for more details.
-**/
+/// Runs the program: directs program arguments, stdout, and stderr to main_impl::main_io_wrapper,
+/// and exits with the resulting exit code or error
+///
+/// See main_impl.rs for more details.
 
 fn main() -> Result<(), Box<dyn Error>> {
 
