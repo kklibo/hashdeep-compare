dcb028f54bdf8b6e64f0167c560d7a5b9f5b1105
kklibo <kklibo4@gmail.com>
2021-04-29 14:14:00 -0700

    refactored run_hashdeep_command to use OpenOptions::create_new

--- src/command.rs
+++ src/command.rs
@@ -1,7 +1,7 @@
 use std::process::{Command,Stdio};
 use std::error::Error;
-use std::fs::File;
-use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::ErrorKind;
 
 pub fn run_hashdeep_command(target_directory: &str, output_path_base: &str) -> Result<(), Box<dyn Error>> {
 
@@ -9,11 +9,56 @@ pub fn run_hashdeep_command(target_directory: &str, output_path_base: &str) -> R
 
     let output_error_path = format!("{}{}", output_path_base, error_log_suffix);
 
-    match ( Path::new(output_path_base).exists(), Path::new(&output_error_path).exists() ) {
-        (true,  false) => return Err(format!("{} exists (will not overwrite existing files)", output_path_base).into()),
-        (false, true ) => return Err(format!("{} exists (will not overwrite existing files)", output_error_path).into()),
-        (true,  true ) => return Err(format!("{} and {} exist (will not overwrite existing files)", output_path_base, output_error_path).into()),
-        (false, false) => (),
+
+    //try to open both output files
+    let maybe_output_file = OpenOptions::new().write(true).create_new(true).open(&output_path_base);
+    let maybe_error_file  = OpenOptions::new().write(true).create_new(true).open(&output_error_path);
+
+
+    let (output_file, error_file) =
+
+    match (maybe_output_file, maybe_error_file) {
+
+        (Ok(output_file), Ok(error_file)) => (output_file, error_file),
+
+
+        //if either file failed to open, abort command and clean up:
+
+        (Err(output_file_error), Ok(_)) => {
+
+            //delete the file that was successfully created
+            std::fs::remove_file(&output_error_path)?;
+
+            return match output_file_error.kind() == ErrorKind::AlreadyExists {
+                true  => Err(format!("{} exists (will not overwrite existing files)", output_path_base).into()),
+                false => Err(output_file_error.into()),
+            }
+        },
+
+        (Ok(_), Err(error_file_error)) => {
+
+            //delete the file that was successfully created
+            std::fs::remove_file(&output_path_base)?;
+
+            return match error_file_error.kind() == ErrorKind::AlreadyExists {
+                true  => Err(format!("{} exists (will not overwrite existing files)", output_error_path).into()),
+                false => Err(error_file_error.into()),
+            }
+        },
+
+        (Err(output_file_error), Err(error_file_error)) => {
+
+            return match ( output_file_error.kind() == ErrorKind::AlreadyExists,
+                            error_file_error.kind() == ErrorKind::AlreadyExists ) {
+
+                (true,  false) => Err(format!("{} exists (will not overwrite existing files)", output_path_base).into()),
+                (false, true ) => Err(format!("{} exists (will not overwrite existing files)", output_error_path).into()),
+                (true,  true ) => Err(format!("{} and {} exist (will not overwrite existing files)", output_path_base, output_error_path).into()),
+
+                //just return the output file's error (todo: do something more useful here?)
+                (false, false) => Err(output_file_error.into()),
+            };
+        }
     };
 
     Command::new("hashdeep")
@@ -24,8 +69,8 @@ pub fn run_hashdeep_command(target_directory: &str, output_path_base: &str) -> R
     .arg(target_directory)
 
     .stdin(Stdio::null())
-    .stdout(File::create(output_path_base)?)
-    .stderr(File::create(output_error_path)?)
+    .stdout(output_file)
+    .stderr(error_file)
 
     .status()?;
     Ok(())
